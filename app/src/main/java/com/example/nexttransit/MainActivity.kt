160d24b8989a6600e481eabb0b0fd88fb4fa2ff2
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2023-10-06 11:33:26 +0200

    Simple widget UI

--- app/src/main/java/com/example/nexttransit/MainActivity.kt
+++ app/src/main/java/com/example/nexttransit/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.nexttransit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nexttransit.ui.theme.NextTransitTheme
 import com.example.nexttransit.ApiCaller.getDirections
 import kotlinx.coroutines.launch
@@ -56,4 +69,31 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+//
+//@Preview(showBackground=true)
+//@Composable
+//fun SimpleDisplay(){
+//    NextTransitTheme {
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(Color.DarkGray),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ){
+//            Row(verticalAlignment = Alignment.CenterVertically){
+//                Text(text="A", style= TextStyle(color = Color.White))
+//                Text(text=" > ", style= TextStyle(color = Color.White))
+//                Text(text="B", style= TextStyle(color = Color.White))
+//                Spacer(Modifier.size(16.dp))
+//                Text(text="16 min")
+//            }
+//            Text(
+//                text="Departure: 21:37",
+//                style= TextStyle(
+//                    color = Color.White,
+//                    fontWeight = FontWeight.Bold,
+//                    fontSize=14.sp
+//                ))
+//        }
+//    }
+//}
--- app/src/main/java/com/example/nexttransit/SimpleDataDisplay.kt
+++ app/src/main/java/com/example/nexttransit/SimpleDataDisplay.kt
@@ -1,5 +1,10 @@
 package com.example.nexttransit
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nexttransit.ui.theme.NextTransitTheme
+
 class SimpleDataDisplay {
 
 }
