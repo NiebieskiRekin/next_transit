36c91025a54f9d4e7829a2ee15adde1b7f31b45d
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-18 13:53:49 +0200

    Directions Query Entity

---
+++ app/src/main/java/com/example/nexttransit/model/DirectionsQuery.kt
@@ -0,0 +1,17 @@
+package com.example.nexttransit.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.nexttransit.model.calendar.Event
+import com.example.nexttransit.model.routes.DirectionsResponse
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Entity
+data class DirectionsQuery (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val firstEvent: Event,
+    val secondEvent: Event,
+    val directionsResponse: DirectionsResponse
+)
