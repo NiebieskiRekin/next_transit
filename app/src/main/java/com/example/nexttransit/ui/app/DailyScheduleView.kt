bd2a404ebc9a1f3023bebe29779a11d49e3469cf
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-18 13:58:32 +0200

    Convert DailyScheduleView to serializable equivalents

--- app/src/main/java/com/example/nexttransit/ui/app/DailyScheduleView.kt
+++ app/src/main/java/com/example/nexttransit/ui/app/DailyScheduleView.kt
@@ -1,59 +1,35 @@
 package com.example.nexttransit.ui.app
 
-import android.content.ContentUris
-import android.content.Intent
-import android.net.Uri
-import android.provider.CalendarContract
-import android.util.Log
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import com.example.nexttransit.model.calendar.Event
-import com.example.nexttransit.model.calendar.ScheduleSlotItem
-import com.example.nexttransit.model.calendar.generateScheduleSlots
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-import java.util.UUID
-import kotlin.collections.sortedBy
+import com.example.nexttransit.model.calendar.TZ
+import com.example.nexttransit.model.calendar.getLocalTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.format
+import kotlinx.datetime.format.DateTimeFormat
 
 /**
  * Composable for displaying an individual event.
@@ -63,7 +39,7 @@ import kotlin.collections.sortedBy
  */
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun EventCard(event: Event, timeFormatter: DateTimeFormatter, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}, isSelected: Boolean) {
+fun EventCard(event: Event, timeFormatter: DateTimeFormat<LocalTime>, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}, isSelected: Boolean) {
     var modifier = Modifier.fillMaxWidth().combinedClickable(
         onClick = onClick,
         onLongClick = onLongClick
@@ -108,7 +84,7 @@ fun EventCard(event: Event, timeFormatter: DateTimeFormatter, onClick: () -> Uni
             )
             Spacer(modifier = Modifier.height(4.dp))
             Text(
-                text = "${event.startDateTime.format(timeFormatter)} - ${event.endDateTime.format(timeFormatter)}",
+                text = "${event.startDateTime.getLocalTime(TZ).format(timeFormatter)} - ${event.endDateTime.getLocalTime(TZ).format(timeFormatter)}",
                 style = MaterialTheme.typography.bodySmall,
                 color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.7f)
             )
@@ -124,7 +100,7 @@ fun EventCard(event: Event, timeFormatter: DateTimeFormatter, onClick: () -> Uni
  * @param timeFormatter Formatter for displaying times.
  */
 @Composable
-fun GapCard(startTime: LocalTime, endTime: LocalTime, timeFormatter: DateTimeFormatter) {
+fun GapCard(startTime: LocalTime, endTime: LocalTime, timeFormatter: DateTimeFormat<LocalTime>) {
 
     val outline = MaterialTheme.colorScheme.outline
     val stroke = Stroke(width = 4f,
