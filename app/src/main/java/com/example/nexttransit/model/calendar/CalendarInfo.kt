5a864b82c1921480fd5a206c5ad451bb1bf41b44
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-18 13:22:36 +0200

    Serializable Calendar

--- app/src/main/java/com/example/nexttransit/model/calendar/CalendarInfo.kt
+++ app/src/main/java/com/example/nexttransit/model/calendar/CalendarInfo.kt
@@ -1,11 +1,16 @@
 package com.example.nexttransit.model.calendar
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Immutable
+import com.example.nexttransit.model.ColorAsIntSerializer
+import kotlinx.serialization.Serializable
 
 @Immutable
+@Serializable
 data class CalendarInfo(
     val id: Long,
     val displayName: String,
     val accountName: String,
     val ownerName: String,
-    val color: Int? = null // Calendar color
+    @Serializable(with = ColorAsIntSerializer::class)
+    val color: Color? = null
 )
